aacaf169e4c4a1646e10fb27d91f34466134e31d
shirleyz9402 <shirleyz9402@yahoo.com>
2018-07-18 15:09:10 -0400

    done

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,11 @@
 /*: question1
  ### 1. Create a changeable list for the five days of the week called 'daysOfTheWeek' and print each one by using a loop.
  */
+var daysOfTheWeek: [String] = ["Monday","Tuesday","Wednesday", "Thursday", "Friday"]
 
+for day in daysOfTheWeek {
+    print(day)
+}
 
 
 
@@ -25,7 +29,11 @@
 /*: question2
  ### 2. Create an unchanging list for the five days of the week called 'weekDays' and print each one by using a loop, prefixed by the position of the day of the week.
  */
+let weekDays:[String] = ["Monday","Tuesday","Wednesday", "Thursday", "Friday"]
 
+for (index, day) in weekDays.enumerated() {
+    print("\(index + 1) \(day)")
+}
 
 
 
@@ -36,7 +44,11 @@
 /*: question3
  ### 3. Create an empty array of strings called 'emptyArray' and check to see if it's empty, printing appropriate messages.
  */
+let emptyArray: [String] = []
 
+if emptyArray.isEmpty {
+    print("empty")
+}
 
 
 
@@ -48,7 +60,12 @@
 /*: question4
  ### 4.  Create an array of strings called 'funColors'. The colors in the array should be "red", "blue" and "green". Check to see if it's NOT empty, printing an appropriate message.
  */
+let funColors = ["red", "blue", "green"]
 
+if !funColors.isEmpty {
+    print("We have the best colors.")
+    
+}
 
 
 
@@ -72,7 +89,8 @@
 /*: question6
  ### 6. Add the two days of the weekend to the 'daysOfTheWeek array and then loop through the array to print all the values.
  */
-
+daysOfTheWeek.append("Saturday")
+daysOfTheWeek.append("Sunday")
 
 
 
@@ -85,8 +103,12 @@
 /*: question7
  ### 7. Some people start the week on a Sunday. Remove Sunday from the end of the 'daysOfTheWeek' array and then re-add it to the beginning of the array.
  */
+daysOfTheWeek.remove(at: daysOfTheWeek.index(of: "Sunday")!)
+daysOfTheWeek.insert("Sunday", at:0)
 
-
+for day in daysOfTheWeek {
+    print(day)
+}
 
 
 
@@ -98,7 +120,11 @@
  */
 
 
-
+for day in daysOfTheWeek {
+    
+    print(day.lowercased())
+    
+}
 
 
 
